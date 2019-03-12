a87b051e261f8bc135374b93c9e37f0cd9531d2a
pmckeegan <pmckeegan@gmail.com>
2019-03-12 01:30:55 +0000

    used JSON.parse to find the url of the avatars

--- download_avatars.js
+++ download_avatars.js
@@ -1,22 +1,33 @@
 var request = require("request");
+var pw = require('./secrets');
+
 
 console.log("Welcome to the GitHub Avatar Downloader!")
 
 function getRepoContributors(repoOwner, repoName, cb){
-var options = {
-  url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
-  headers: {
-    'User-Agent': "pmckeegan"
-  }
-};
+  var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+      'User-Agent': 'pmckeegan',
+      'Authorization': 'GITHUB_TOKEN'
+    }
+  };
+  request(options, function(err, res, body) {
+    //cb(err, body);
 
+    var url = JSON.parse(body, function (key, location){
+      if (key == "avatar_url"){
+        cb(key, location);
+       console.log("Avatar URL: ", location);
+     }
+   });
 
-request(options, function(err, res, body) {
-  cb(err, body);
-});
+    //cb(url.avatar_url);
+  });
 }
 
 getRepoContributors("jquery", "jquery", function(err, result){
-console.log("errors:", err);
-console.log("Results:", result);
+  //console.log("errors:", err);
+  //console.log("Results:", result);
 });
+
