f28580ec0c23a0a75b2e28de24efa0b313c024f8
pmckeegan <pmckeegan@gmail.com>
2019-03-16 22:22:41 +0000

    finalized code for review

--- download_avatars.js
+++ download_avatars.js
@@ -1,10 +1,11 @@
 
-//set requirements
+//set app requirements, global variables.
 var request = require("request");
 var fs = require("fs");
 var pw = require("./secrets");
 var input = process.argv.slice(2);
 
+//begin
 console.log("Welcome to the GitHub Avatar Downloader!");
 
 //find image filepaths
@@ -15,37 +16,34 @@ function getRepoContributors(repoOwner, repoName, cb) {
         "/" +
         repoName +
         "/contributors",
+//authentication for GitHub
     headers: {
       "User-Agent": "pmckeegan",
       "Authorization": "GITHUB_TOKEN"
-    }
-   
+    } 
   };
- 
+ //parse user data
   request(options, function(err, res, body) {
     var data = JSON.parse(body);
       cb(err, data);
   });
-
-  console.log(options);
 }
 //get files
-       getRepoContributors(input[0], input[1], function(err, result) {
+getRepoContributors(input[0], input[1], function(err, result) {
         
-        if (!input[0] || !input[1]) {
-          console.log("please add the user and repo name you'd like to use.");
-        } else {
-        for (var i = 0; i < result.length; i++) {
-          downloadImageByURL(
-            result[i].avatar_url,
-            "./avatars/" + result[i].login + ".jpg");
-        }
-      }
-      console.log("errors:", err);
-      console.log("results:", result);
-        });
-      function downloadImageByURL(url, filePath) {
-      request(url).pipe(fs.createWriteStream(filePath));
-      }
+  if (!input[0] || !input[1]) {
+    console.log("please add the user and repo name you'd like to use.");
+  } else {
+    for (var i = 0; i < result.length; i++) {
+      downloadImageByURL(
+      result[i].avatar_url,
+      "./avatars/" + result[i].login + ".jpg");
+    }
+  }
+    console.log("errors:", err);
+});
+    function downloadImageByURL(url, filePath) {
+    request(url).pipe(fs.createWriteStream(filePath));
+}
 
 
